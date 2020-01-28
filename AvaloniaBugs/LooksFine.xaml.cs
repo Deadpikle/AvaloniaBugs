1b2e95cb253c8d35419bb917f72842b50f881d35
Deadpikle <mbabienco@southern.edu>
2020-01-28 11:24:14 -0500

    Add drag drop datagrid

---
+++ AvaloniaBugs/LooksFine.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DataGridPreview
+{
+    public class LooksFine : UserControl
+    {
+        public LooksFine()
+        {
+            this.InitializeComponent();
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ DragDropDataGrid/MainWindow.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
+
+namespace DragDropDataGrid
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            DataContext = new List<string>()
+            {
+                "Hi",
+                "There",
+                "How",
+                "Are",
+                "You"
+            };
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
