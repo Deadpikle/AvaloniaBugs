16e23e48e042bc7cb7b32a0d2925ff08750a4ae3
Deadpikle <mbabienco@southern.edu>
2020-03-10 13:45:59 -0400

    Add bug with italic text wrapping on latest nuget

---
+++ ItalicTextWrapping/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace ItalicTextWrapping
+{
+    public class MainWindow : Window, INotifyPropertyChanged
+    {
+        private List<string> _textItems;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            //this.AttachDevTools();
+#endif
+            TextItems = new List<string>()
+            {
+                "Whether to turn off HTTPS. This option only applies if Individual, IndividualB2C, SingleOrg, or MultiOrg aren't used for --auth.",
+            };
+        }
+
+        public List<string> TextItems
+        {
+            get => _textItems;
+            set { _textItems = value; NotifyPropertyChanged(); }
+        }
+
+        public new event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
