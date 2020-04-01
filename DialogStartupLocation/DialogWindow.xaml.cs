f8a766bfdb069c435d06d51399d2aa2603bf8b81
Deadpikle <mbabienco@southern.edu>
2020-04-01 11:33:27 -0400

    Add repro for Avalonia #2859

---
+++ DialogStartupLocation/DialogWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace DialogStartupLocation
+{
+    public class DialogWindow : Window
+    {
+        public DialogWindow()
+        {
+            this.InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ DialogStartupLocation/MainWindow.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Threading;
+using System;
+
+namespace DialogStartupLocation
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            var timer = new DispatcherTimer { Interval = TimeSpan.FromSeconds(2) };
+            timer.Start();
+            timer.Tick += (sender, args) =>
+            {
+                timer.Stop();
+                var dialogWindow = new DialogWindow();
+                dialogWindow.ShowDialog(this);
+            };
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
