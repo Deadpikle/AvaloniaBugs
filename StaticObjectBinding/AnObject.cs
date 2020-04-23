4c337c94c71b694065e7b87e9c210ad240b40295
Deadpikle <mbabienco@southern.edu>
2020-04-23 11:57:50 -0400

    Add sample for null ref on static property

---
+++ StaticObjectBinding/AnObject.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace StaticObjectBinding
+{
+    class AnObject
+    {
+        public static event PropertyChangedEventHandler StaticPropertyChanged;
+
+        private static void OnStaticPropertyChanged(string propertyName)
+        {
+            StaticPropertyChanged?.Invoke(null, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private static bool _myProperty = true;
+
+        public static bool MyProperty
+        {
+            get => _myProperty;
+            set
+            {
+                _myProperty = value;
+                OnStaticPropertyChanged(nameof(MyProperty));
+            }
+        }
+    }
+}
