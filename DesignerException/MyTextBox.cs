f7982e5f9828fd7aaefa9beedcf47c304b3042b6
Deadpikle <mbabienco@southern.edu>
2020-05-07 14:45:41 -0400

    Add repro for designer exception on custom control

---
+++ DesignerException/MyTextBox.cs
@@ -0,0 +1,14 @@
+﻿using Avalonia.Controls;
+using Avalonia.Styling;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignerException
+{
+	class MyTextBox : TextBox, IStyleable
+	{
+		Type IStyleable.StyleKey => typeof(TextBox);
+
+	}
+}
