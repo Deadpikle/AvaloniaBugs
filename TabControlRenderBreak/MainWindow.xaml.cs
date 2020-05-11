6a2a97c729abbc07e092cdaec7698daf2990618c
Deadpikle <mbabienco@southern.edu>
2020-05-11 16:53:20 -0400

    Add TabControlRenderBreak

---
+++ TabControlRenderBreak/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+
+namespace TabControlRenderBreak
+{
+    public class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = new ViewModel();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+}
---
+++ TabControlRenderBreak/ViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace TabControlRenderBreak
+{
+    class ViewModel
+    {
+        public ViewModel()
+        {
+            Tabs = new List<string>() {};
+        }
+
+        public List<string> Tabs { get; set; }
+    }
+}
