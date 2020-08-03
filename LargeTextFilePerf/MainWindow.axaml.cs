780e5d97264d6261f87957cb1648c710f28de067
Deadpikle <mbabienco@southern.edu>
2020-08-03 19:24:50 -0400

    Add large file text perf

---
+++ LargeTextFilePerf/MainWindow.axaml.cs
@@ -0,0 +1,31 @@
+﻿using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System.IO;
+
+namespace LargeTextFilePerf
+{
+    public class MainWindow : Window
+    {
+        private TextBlock _licenseText;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+#if DEBUG
+            this.AttachDevTools();
+#endif
+            using (TextReader tr = new StreamReader(@"Licenses.txt"))
+            {
+                string myText = tr.ReadToEnd();
+                _licenseText.Text = myText;
+            }
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+            _licenseText = this.Find<TextBlock>("LicensesText");
+        }
+    }
+}
