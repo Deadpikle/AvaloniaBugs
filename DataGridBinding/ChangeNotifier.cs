2ccf6564cb4125f6874b20ce10d7db8715afa45e
Deadpikle <mbabienco@southern.edu>
2021-05-16 12:25:19 -0400

    Data grid binding bug

---
+++ DataGridBinding/ChangeNotifier.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+
+namespace DataGridBinding
+{
+    // https://docs.microsoft.com/en-us/dotnet/api/system.componentmodel.inotifypropertychanged?view=netframework-4.7.2
+    class ChangeNotifier : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
---
+++ DataGridBinding/MainViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridBinding
+{
+    class MainViewModel : ChangeNotifier
+    {
+        private int _selectedIndex;
+        private string _selectedItem;
+
+        private List<string> _items;
+
+        public MainViewModel()
+        {
+            _selectedIndex = -1;
+            _selectedItem = "";
+            _items = new List<string>()
+            {
+                "one",
+                "two",
+                "three"
+            };
+        }
+
+        public int SelectedIndex
+        {
+            get => _selectedIndex;
+            set { _selectedIndex = value; NotifyPropertyChanged(); NotifyPropertyChanged(nameof(SelectedIndexMessage)); }
+        }
+
+        public string SelectedItem
+        {
+            get => _selectedItem;
+            set { _selectedItem = value; NotifyPropertyChanged(); }
+        }
+
+
+        public string SelectedIndexMessage
+        {
+            get => _selectedIndex == -1 ? "Nothing selected" : "You've selected " + _selectedIndex;
+        }
+
+        public List<string> Items
+        {
+            get => _items;
+            set { _items = value; NotifyPropertyChanged(); }
+        }
+    }
+}
